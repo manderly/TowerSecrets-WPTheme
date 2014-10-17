bf456bb74f17c7155fcd6fa6fe083e1c886a3444
Mandi Rocket <mandi018@gmail.com>
2014-10-16 18:34:10 -0700

    Wrote a custom loop to make the posts appear as just links in an unordered list when viewing a category page, added styles.

--- 13thFloor-Genesis-TowerSecrets/functions.php
+++ 13thFloor-Genesis-TowerSecrets/functions.php
@@ -42,3 +42,53 @@ function lm_dequeue_footer_styles()
 {
   wp_dequeue_style('yarppRelatedCss');
 }
+
+
+//Make it so category pages are just a list of titles as links
+//http://sridharkatakam.com/show-post-titles-category-pages-genesis/
+add_action( 'pre_get_posts', 'mjg_show_titles_only_category_pages' );
+/**
+ * Show Linked Titles Only for Posts in Category Pages
+ *
+ * @author Sridhar Katakam
+ * @author Bill Erickson
+ * @link http://www.billerickson.net/customize-the-wordpress-query/
+ * @param object $query data
+ *
+ */
+function mjg_show_titles_only_category_pages( $query ) {
+ 
+	if( $query->is_main_query() && $query->is_category() ) {
+		$query->set( 'orderby', 'title' );
+		$query->set( 'order', 'ASC' );
+		// $query->set( 'posts_per_page', '2' );
+ 
+		//* Remove the post info
+		remove_action( 'genesis_before_post_content', 'genesis_post_info' );
+ 
+		//* Remove the post thumbnail
+		remove_action( 'genesis_post_content', 'genesis_do_post_image' );
+ 
+		//* Remove the post
+		remove_action( 'genesis_post_content', 'genesis_do_post_content' );
+		remove_action( 'genesis_post_content', 'genesis_do_post_permalink' );
+		remove_action( 'genesis_post_content', 'genesis_do_post_content_nav' );
+ 
+		//* Remove the post meta
+		remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
+		
+		remove_action( 'genesis_loop', 'genesis_do_loop' );
+		add_action( 'genesis_loop', 'mjg_custom_loop' );
+	}
+ 
+}
+ 
+function mjg_custom_loop() {
+ 
+ 	echo '<ul class="category-post-title-list">';
+	while (have_posts()) : the_post();
+		?>
+		<li><a href="<?php the_permalink() ?>"><?php the_title() ?></a></li>
+	<?php
+	endwhile;
+}
