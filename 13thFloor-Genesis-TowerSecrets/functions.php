c89e14787190d86d1b7cc20c41083e9953b866e8
Mandi Grant <mandi018@gmail.com>
2019-06-06 20:39:48 -0500

    Updated link to my portfolio site and changed http:// to https://

--- 13thFloor-Genesis-TowerSecrets/functions.php
+++ 13thFloor-Genesis-TowerSecrets/functions.php
@@ -4,7 +4,7 @@ require_once( get_template_directory() . '/lib/init.php' );
 
 // Child theme (do not remove)
 define( 'CHILD_THEME_NAME', '13th Floor - Genesis Child Theme' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_URL', 'https://www.studiopress.com/' );
 
 // Add Viewport meta tag for mobile browsers and starts the site zoomed to 1.0
 //removal candidate
@@ -25,16 +25,16 @@ add_theme_support( 'genesis-custom-header', array(
 //Add a clickable image as the header
 function inject_clickable_header(){ ?>
 <div id="title-area">
-<a href="http://towersecrets.com/"><img src="http://towersecrets.com/wp-content/uploads/2014/10/cropped-tower_secrets_banner.png"/></a>
+<a href="https://towersecrets.com/"><img src="https://towersecrets.com/wp-content/uploads/2014/10/cropped-tower_secrets_banner.png"/></a>
 </div>
 <?php }
 add_action('genesis_header','inject_clickable_header');
 
 
-// Adding custom Favicon 
+// Adding custom Favicon
 add_filter( 'genesis_pre_load_favicon', 'custom_favicon' );
 function custom_favicon( $favicon_url ) {
-    return 'http://towersecrets.com/favicon.ico';
+    return 'https://towersecrets.com/favicon.ico';
 }
 
 // Add support for 3-column footer widget
@@ -65,18 +65,18 @@ function lm_dequeue_footer_styles()
 //Custom loop added 10/16/2014
 add_action( 'pre_get_posts', 'mjg_show_titles_only_category_pages' );
 function mjg_show_titles_only_category_pages( $query ) {
- 
+
 	if( $query->is_main_query() && $query->is_category() ) {
 		$query->set( 'orderby', 'title' );
 		$query->set( 'order', 'ASC' );
-		
-		
-		
+
+
+
 		remove_action( 'genesis_loop', 'genesis_do_loop' );
 		add_action( 'genesis_loop', 'mjg_custom_loop' );
 	}
 }
- 
+
 function mjg_custom_loop() {
 	echo '<div class="category-title">';
 	single_cat_title( 'Category: ', true );
@@ -97,7 +97,7 @@ function mjg_footer() {
     <div id="bottom-footer">
     <p>&copy; Copyright 2014</p>
     </br>
-    <p>TowerSecrets is rockin' "13th Floor", a custom <a href="https://wordpress.org/">WordPress</a> theme for the <a href="http://my.studiopress.com/themes/genesis/">Genesis framework</a>. Site design by <a href="http://mjgrant.org">M. Grant</a></p>
+    <p>TowerSecrets is rockin' "13th Floor", a custom <a href="https://wordpress.org/">WordPress</a> theme for the <a href="https://my.studiopress.com/themes/genesis/">Genesis framework</a>. Site design by <a href="https://majoh.dev">M. Grant</a></p>
     </div>
     <?php
 }
